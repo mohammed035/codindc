b17760da5860fa433b64741d0d75377a60ee6a3c
mohammed035 <42632631+mohammed035@users.noreply.github.com>
2020-07-24 12:48:44 +0530

    added conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
              public static void main(String args[]){
                        while(true){
-                              System.out.println("hello world");
+                              System.out.println("12hello world");
                               try{
                                    Thread.sleep(60000);
                                      }catch(Exception ex){
