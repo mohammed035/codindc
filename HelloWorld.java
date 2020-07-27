b5e7461bee48c2bdcffa15d67c9b8bff6af8d4bf
mohammed035 <mohammedabbas035@gmail.com>
2020-07-27 12:01:50 +0530

    new line in feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
              public static void main(String args[]){
                        while(true){
                               System.out.println("this is my master branch")
-                              
+                             system.out.print ln ("new linw in feature") 
                               try{
                                    Thread.sleep(60000);
                                      }catch(Exception ex){
