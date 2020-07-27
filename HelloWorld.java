6eca06332f16b7e7bc6f6671a1476d0e277ae823
mohammed035 <mohammedabbas035@gmail.com>
2020-07-27 12:33:55 +0530

    cheking error log

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
              public static void main(String args[]){
                        while(true){
-                              System.out.println("this is my master branch"
+                              System.out.println("this is my master branch 11")
                              system.out.print ln ("new linw in feature") 
                               Systme,out,println("new branch develop")
                               try{
