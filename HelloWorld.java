41ca25890109d8164222b676a2329ed615b85c0b
mohammed035 <mohammedabbas035@gmail.com>
2020-07-27 11:52:55 +0530

    added in develop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
              public static void main(String args[]){
                        while(true){
                               System.out.println("this is my master branch")
-                              
+                              Systme,out,println("new branch develop")
                               try{
                                    Thread.sleep(60000);
                                      }catch(Exception ex){
